19984af87a879a85048bc895ec5ef2fb5d1d1411
kim <kim@3sixty.co.uk>
2021-05-18 15:56:01 +0100

    projects config

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,12 +63,12 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
+              <Link className="navbar-item" to="/projects">
+                Projects
               </Link>
-              <Link className="navbar-item" to="/blog">
+              {/* <Link className="navbar-item" to="/blog">
                 Blog
-              </Link>
+              </Link> */}
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
@@ -77,7 +77,7 @@ const Navbar = class extends React.Component {
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
-              <a
+              {/* <a
                 className="navbar-item"
                 href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
                 target="_blank"
@@ -86,7 +86,7 @@ const Navbar = class extends React.Component {
                 <span className="icon">
                   <img src={github} alt="Github" />
                 </span>
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
---
+++ src/templates/projects.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Layout from '../components/Layout'
+
+
+
+
+
+const Projects = () => {
+  
+  return (
+    <Layout>
+      <h2>Projects</h2>
+    </Layout>
+  )
+}
+
+
+
+export default Projects
+
